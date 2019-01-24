f86e3ca9aa73351665157d81148c00ccfaa8ffb1
Claudwin <claudwin.fortune@gmail.com>
2019-01-24 12:25:08 -0700

    Added third project & rss feed for medium post

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,7 @@ jQuery(document).ready(function($) {
     $("#rss-feeds").rss(
     
         //Change this to your own rss feeds
-        "https://feeds.feedburner.com/TechCrunch/startups",
+        "https://medium.com/feed/@claudwin.fortune",
         
         {
         // how many entries do you want?
